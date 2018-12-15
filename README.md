4f76e60e5eece29360d63491195f1aa526c620a3
Svetlana Tislenko <contextnorm@gmail.com>
2018-12-15 12:07:54 +0200

    remove .idea

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-basic
+
+ternaty operator construction
+
+if else construction
+
+switch case construction
---
+++ assets/js/simple-calculator.js
@@ -0,0 +1,57 @@
+(() => { //(function() {...} for ES5
+
+    setTimeout( () => {
+    const a = Number(prompt('Enter first argument!'));
+    const b = Number(prompt('Enter second argument!'));
+    const action = prompt('Enter action as arithmetical operation');
+
+    /**  ternaty operator construction  */
+
+    '+' === action
+        ? alert(a + ' + ' + b + ' = ' + (a + b))
+        : ('-' === action)
+            ? alert(a + ' - ' + b + ' = ' + (a - b))
+            : ('*' === action)
+              ? alert(a + ' * ' + b + ' = ' + (a * b))
+              : alert('Unavailable action!')
+    ;
+
+    /**  if else construction */
+
+    // if ('+' === action) {
+    //     alert(a + ' + ' + b + ' = ' + (a + b));
+    // } else  if ('-' === action) {
+    //     alert(a + ' - ' + b + ' = ' + (a - b));
+    // }
+    // else  if ('*' === action) {
+    //     alert(a + ' * ' + b + ' = ' + (a * b));
+    // }
+    // else  if ('%' === action) {
+    //     alert(a + ' % ' + b + ' = ' +  (a % b));
+    // } else  {
+    //     alert('Unavailable action');
+    // }
+
+     /**  switch case construction */ 
+
+    // switch (action) {
+    //     case '+':
+    //         alert(a + ' + ' + b + ' = ' + (a + b));
+    //         break;
+    //     case '-':
+    //         alert(a + ' - ' + b + ' = ' + (a - b));
+    //         break;
+    //     case '*':
+    //         alert(a + ' * ' + b + ' = ' + (a * b));
+    //         break;
+    //     case '/':
+    //         alert(a + ' / ' + b + ' = ' + (a / b));
+    //         break;
+    //     default:
+    //         alert('Unavailable action');
+    //         break;
+    // }
+
+    }, 10);
+
+}) ();
